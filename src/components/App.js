730c249874d47e1623e6eb6a3cab3bee236e6e07
nateos <sodjanathan@gmail.com>
2021-02-03 21:07:16 +0000

    Reconfigured Setup Created App and Todo Components Passed Prop From App Component to Todo Component

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import Todos from '../components/Todos'
+
+class App extends Component {
+
+  state = {
+    todos: [{
+      id: 1,
+      title: 'Take out the trash',
+      completed: false
+    },{
+      id: 2,
+      title: 'Code',
+      completed: false
+    },{
+      id: 3,
+      title: 'Sleep',
+      completed: false
+    }]
+  }
+
+  render() {
+    console.log(this.state.todos)
+    return (
+      <div>
+        <h1>App</h1>
+        <Todos todos={this.state.todos}/> 
+      </div>
+    )
+  }
+}
+
+export default App;
